aef9f0887b8839db0bfee1baaee271b2a5377452
AlanMiranda-BR <alan_pipemaster@hotmail.com>
2019-03-30 10:23:52 -0300

    fix Cliente DELETE with cascade

--- src/main/java/com/cursospring/cursomc/domain/Cliente.java
+++ src/main/java/com/cursospring/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -31,7 +32,7 @@ public class Cliente implements Serializable {
 	private Integer tipo; //Tipo alterado
 	
 	//Atributo da associação cliente_enderecos
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	//Implementação de entidade fraca como parte da classe que associa
