368ca86f224fb1bd2fb01f10f5bfbe07d2772579
AlanMiranda-BR <alan_pipemaster@hotmail.com>
2019-03-30 14:45:56 -0300

    add email custom validation on INSERT Cliente.

--- src/main/java/com/cursospring/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/cursospring/cursomc/repositories/ClienteRepository.java
@@ -2,8 +2,14 @@ package com.cursospring.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cursospring.cursomc.domain.Cliente;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Integer>{}
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email); // habilita busca por email (obs. basta escrever a sintaxe corretamente que o Spring já implementa o método de busca sozinho)
+	
+}
--- src/main/java/com/cursospring/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursospring/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursospring.cursomc.domain.Cliente;
 import com.cursospring.cursomc.domain.enums.TipoCliente;
 import com.cursospring.cursomc.dto.ClienteNewDTO;
+import com.cursospring.cursomc.repositories.ClienteRepository;
 import com.cursospring.cursomc.resources.exceptions.FieldMessage;
 import com.cursospring.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +35,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+			
+		//validação do email
+		Cliente aux = repo.findByEmail(objDTO.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		
 		// Os teste terminam aqui
 		for (FieldMessage e : list) {
@@ -37,5 +50,5 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		return list.isEmpty(); // Se a lista de erros estiver vazia retorna TRUE.
 	}
-
+	
 }
